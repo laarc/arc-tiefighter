2ea9dfa4296ae26ff0b1e22655e373efc57ea245
laarc <sudoarc@gmail.com>
2015-10-20 06:07:29 -0700

    motor/monki.l: rename system.l to platform.l; copy lua libs to ../lib/;  lumen/monki.l: add lib/${os} to LUA_PATH and NODE_PATH

---
+++ lib/buffer.lua
@@ -0,0 +1,50 @@
+local ffi = require("ffi")
+local default_size = 4096
+local function allocate(n)
+  return(ffi.new("char[?]", n))
+end
+local function create(n)
+  local _n = n or default_size
+  local _x = {}
+  _x.capacity = _n
+  _x.length = 0
+  _x.storage = allocate(_n)
+  return(_x)
+end
+local function length(b)
+  return(b.length)
+end
+local function extend(b, n)
+  local _e
+  if n then
+    _e = b.capacity + n
+  else
+    _e = b.capacity * 2
+  end
+  local _n1 = _e
+  local x = allocate(_n1)
+  ffi.copy(x, b.storage, b.length)
+  b.storage = x
+  b.capacity = _n1
+  return(b.capacity)
+end
+local function full63(b)
+  return(b.length == b.capacity)
+end
+local function pointer(b)
+  return(b.storage + b.length)
+end
+local function space(b)
+  return(b.capacity - b.length)
+end
+local function string(b, i, n)
+  local _i = i or 0
+  local max = b.length - _i
+  local _n2 = min(n or max, max)
+  if _i < b.length then
+    return(ffi.string(b.storage + _i, _n2))
+  else
+    return("")
+  end
+end
+return({string = string, space = space, pointer = pointer, length = length, ["full?"] = full63, create = create, extend = extend})
---
+++ lib/stream.lua
@@ -0,0 +1,75 @@
+local motor = require("motor")
+local buffer = require("buffer")
+local function create(fd)
+  local _x = {}
+  _x.fd = fd
+  _x.pos = 0
+  _x.buffer = buffer.create()
+  return(_x)
+end
+local function space(s)
+  return(buffer.space(s.buffer))
+end
+local function length(s)
+  return(buffer.length(s.buffer))
+end
+local function full63(s)
+  return(buffer.full63(s.buffer))
+end
+local function extend(s, n)
+  return(buffer.extend(s.buffer, n))
+end
+local function read(s)
+  return(motor.read(s.fd, s.buffer))
+end
+local function string(s, n)
+  return(buffer.string(s.buffer, s.pos, n))
+end
+local function fill(s)
+  if full63(s) then
+    extend(s)
+  end
+  return(read(s) > 0)
+end
+local function before(s, pat)
+  local n = nil
+  while nil63(n) do
+    local x = string(s)
+    local m = search(x, pat)
+    if nil63(m) then
+      if not fill(s) then
+        n = -1
+      end
+    else
+      n = m
+    end
+  end
+  if n >= 0 then
+    local _x1 = string(s, n)
+    s.pos = s.pos + n
+    return(_x1)
+  end
+end
+local function line(s, pat)
+  local p = pat or "\n"
+  local x = before(s, p)
+  s.pos = s.pos + _35(p)
+  return(x)
+end
+local function take(s, n)
+  if space(s) < n then
+    extend(s, n)
+  end
+  while length(s) - s.pos < n do
+    if not fill(s) then
+      break
+    end
+  end
+  local x = string(s, n)
+  s.pos = s.pos + _35(x)
+  return(x)
+end
+local function emit(s, b)
+  return(motor.send(s.fd, b))
+end
+return({line = line, emit = emit, create = create, take = take})
